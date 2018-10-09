89dc1a3dcedfa90bf642b74cc0ac508d3d716cf9
Emmanuel Mahuni <emahuni@gmail.com>
2018-10-09 13:22:30 +0200

    feat(extend): add patching of undefined extObj

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ module.exports = function extendModule(modulesNames, extObj, ...extraExtObjs) {
 
   let extent = {};
 
-  // use each module name to require a unique copy of the module and then merge it into the extent obj
+  // use each module name to require the module and then merge it into the extent obj
   for(let modNam of modulesNames) {
     if (!_.isString(modNam)) {
       throw new Error(`not a string` + util.inspect(modNam));
@@ -20,9 +20,12 @@ module.exports = function extendModule(modulesNames, extObj, ...extraExtObjs) {
 
     // console.debug('modNam: ', modNam);
     // get and merge the modules into extent
-    _.merge(extent, _.cloneDeep(require(modNam)));
-    // _.merge(extent, require(modNam));
+    // _.merge(extent, _.cloneDeep(require(modNam)));
+    _.merge(extent, require(modNam));
   }
+
+  // just make sure we have something that's not undefined if undefined
+  extObj = extObj || {};
 
   // do final merge with the extending objects
   _.merge(extent, extObj, ...extraExtObjs);
