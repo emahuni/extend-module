e87b737756f0abce589e100e5a02a20d00e9ccd2
Emmanuel Mahuni <emahuni@gmail.com>
2018-10-23 14:59:39 +0200

    fix(main): fix require path resolution

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const _ = require('lodash');
 const console = require('contrace')();
 const util = require('util');
+const path = require('path');
 
 module.exports = function extendModule(modulesNames, extObj, ...extraExtObjs) {
   // make sure that modulesNames is an array
@@ -21,7 +22,7 @@ module.exports = function extendModule(modulesNames, extObj, ...extraExtObjs) {
     // console.debug('modNam: ', modNam);
     // get and merge the modules into extent
     // _.merge(extent, _.cloneDeep(require(modNam)));
-    _.merge(extent, require(modNam));
+    _.merge(extent, require(require.resolve(modNam, { paths: [ path.dirname(module.parent.filename) ] })));
   }
 
   // just make sure we have something that's not undefined if undefined
